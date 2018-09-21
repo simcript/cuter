2efab05478523abd830ecd5f7eab01d79eab6002
AliA_MehR <alia_mehr@yahoo.com>
2018-09-22 00:49:56 +0330

    use Barryvdh\Cors laravel package

--- config/app.php
+++ config/app.php
@@ -160,6 +160,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Barryvdh\Cors\ServiceProvider::class,
+
 
     ],
 
